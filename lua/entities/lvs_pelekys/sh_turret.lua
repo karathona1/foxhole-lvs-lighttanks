47ae97c68d333ac35329f83bb3223d5153b34d7c
Kalamari <arikarathona@hotmail.com>
2024-07-22 11:47:12 +0300

    yaw calculation

--- lua/entities/lvs_pelekys/sh_turret.lua
+++ lua/entities/lvs_pelekys/sh_turret.lua
@@ -6,11 +6,13 @@ ENT.TurretAimRate = 15
 ENT.TurretRotationSound = "vehicles/tank_turret_loop1.wav"
 
 ENT.TurretPitchPoseParameterName = "turret_pitch"
-ENT.TurretPitchMin = -30
+ENT.TurretPitchMin = -20
 ENT.TurretPitchMax = 10
 ENT.TurretPitchMul = 0.5
 ENT.TurretPitchOffset = 0
 
 ENT.TurretYawPoseParameterName = "turret_yaw"
+ENT.TurretYawMin = -20
+ENT.TurretYawMax = 20
 ENT.TurretYawMul = 1
 ENT.TurretYawOffset = 0
